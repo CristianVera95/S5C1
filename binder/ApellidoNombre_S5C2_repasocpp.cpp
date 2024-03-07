5c068b28b1e31a140482692cec1830cfed646c6a
CristianVera95 <162343949+CristianVera95@users.noreply.github.com>
2024-03-07 10:33:49 -0500

    Add files via upload

---
+++ binder/ApellidoNombre_S5C2_repasocpp.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include<time.h>
+#include<cmath>
+
+/*
+1) Inicializar dos variables globales (con valores escogidos por ustedes), una entera y otra flotante.
+
+2) Imprimir los valores de las variables en un mensaje: "la primera tiene un valor de XX y la segunda variable tiene un valor de YY"
+
+3) Calcular el valor de la segunda variable dividida por la primera e imprimir : "El resultado es ZZ" 
+
+4) Crear un arreglo con 300 números enteros aleatorios entre 0 y 900. 
+(ejemplos en: https://www.geeksforgeeks.org/rand-and-srand-in-ccpp/ o https://www.cplusplus.com/reference/cstdlib/rand/ o rand - C++ Reference entre otros)
+
+5) Hacer una iteración para recorrer dicho arreglo e imprimir todos sus elementos. 
+
+6) Imprimir el quinto elemento del arreglo
+
+7) Obtener la longitud del arreglo e imprimir: "la longitud del arreglo es XX" (use la función size(), mire cppreference)
+
+8) Haga una función que reciba dos variables una flotante llamada mivarflotante y una entera llamada mivarentera y retorne la variable flotante dividida por la variable entera.    
+                    
+9) Llame su función desde la función main e imprima lo que retorna su función para mivarflotante=17.5 y mivarentera=5.
+
+10) Haga una función que retorne el mínimo del arreglo de números aleatorios antes creado
+
+11) Haga una función que reciba el arreglo de números aleatorios antes creado, imprima los números impares y pare de imprimir al encontrar un número mayor a 800
+
+*/
+
+
+
+
+
+
+/*para ver mas ejemplos: https://www.w3schools.com/CPP/cpp_variables.asp*/
+
+
+
+
+
